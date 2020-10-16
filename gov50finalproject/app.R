0dbf05ada693fc0604f493ae4aec5c768582da38
Buddy Scott <jamesscott@college.harvard.edu>
2020-10-16 11:38:00 -0400

    added new tab

--- gov50finalproject/app.R
+++ gov50finalproject/app.R
@@ -1,4 +1,3 @@
-
 library(shiny)
 library(tidyverse)
 library(shiny)
@@ -7,25 +6,7 @@ library(dplyr, warn.conflicts = FALSE)
 library(ggplot2)
 library(ggforce)
 library(readr)
-
-
-######################################################################################
-######################################################################################
-# 
-# 1. Shiny Apps have two basic parts to them
-# 
-#   - The user interface (UI) defines how the app should look.
-#
-#     -- For example, the text on the page, the placement of the elements, etc.
-#
-#   - The server defines how the app should behave.
-#
-#     -- This is how live elements are updated - like selecting a state from a list.
-#
-#   - Those two pieces are combined by running shinyApp(ui, server) to create the app.
-# 
-#      -- You can also click the green "Run App" button on the top right or 
-#         run runApp() in the console
+library(DT)
 
 # reading in data sets
 nbainfo
@@ -38,25 +19,32 @@ ui <- navbarPage(
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
-             p("This project will attempt to analyze the potential 
-             ramifications of the ongoing COVID-19 pandemic for the finances of 
-             NBA teams and the market for NBA players. NBA Commissioner Adam 
-             Silver revealed in May 2020 that ticket sales for in-person fan 
-             attendances comprises an estimated 40% of the NBA’s revenue. The 
-             NBA’s revenue is split roughly 50-50 between players and the 
-             owners of these teams. If this holds true, both the players’ and 
-             the owners’ share of Basketball Related Income would decrease from 
-             $4 billion to $2.4 billion. If this is the case, the league might 
-             have to withhold an increased percentage of players’ salaries in 
-             escrow for the upcoming season to finance the league’s operations, 
-             potentially leading to heavily contested debates at the bargaining
+             p("This project will analyze the potential ramifications of the 
+             ongoing COVID-19 pandemic on the finances of National Basketball
+             Association (NBA) teams and the market for NBA players. NBA 
+             Commissioner Adam Silver revealed in May 2020 that ticket sales for 
+             in-person fan attendance comprises an estimated 40% of the NBA’s 
+             revenue.In a doomdsay scenario that would have no fans in the 
+             arenas for next season, both the players’ and the owners’ share of 
+             Basketball Related Income would decrease from $4 billion to $2.4 
+             billion. If this is the case, the league might have to withhold an 
+             increased percentage of players’ salaries in escrow for the 
+             upcoming season to finance the league’s operations, potentially 
+             leading to heavily contested debates at the bargaining
              table for an updated Collective Bargaining Agreement that has led 
-             to a lockout in the past. The two main topics covered will be the 
+             to lockouts in the past. The two main topics covered will be the 
              league’s potentially inaccurate forward guidance provided to teams 
              about salary cap levels for the upcoming season, as well as 
              potential inequalities emerging from teams’ varying levels of cash 
              flows and spending power that could potentially harm competitive 
              balance in a zero-sum league."),
+             h3("Data Sources"),
+             p("Most of the data used is from a massive spreadsheet that I
+               update that has updated NBA rosters, player contracts, and info
+               about each team. The two external data sources I pulled were
+               salary data from Basketball Reference to complement the salary 
+               data that I maintain, as well as information from Forbes 
+               about the wealth of the majority owners of these 30 NBA teams."),
              h3("About Me"),
              p("My name is Buddy Scott and I study Economics with a secondary 
               in Government at Harvard College. 
@@ -66,33 +54,49 @@ ui <- navbarPage(
              br(),
              a("Please see my GitHub repo here", 
                href = "https://github.com/buddyscott/finalproject"),
-             mainPanel(img(src = "nbalogo.png", height = 140, width = 400))
+             br(), 
+             a("Please see my NBA spreadsheet work here", 
+               href = "https://tinyurl.com/buddyscottnba")
              ),
     
-    tabPanel("Model",
+    tabPanel("NBA Team Info",
              mainPanel(
                  p("This section allows you to plot two variables with each 
                    other, either as a scatterplot, bar graph, or jittered plot."),
                  br(),
                  p("1. Try plotting 'team' as the x variable, 'valuation' as 
-                   the y variable, and 'column' as the geom. Notice the wide 
-                   range of team values (meaning how much the team) is worth. 
-                   The y axis is in billions of dollars."),
+                   the y variable (you have to scroll down a bit), and 'column'
+                   as the geom. Notice the wide range of team values (meaning 
+                   how much the team) is worth. The y axis is in billions of 
+                   dollars."),
+                 p("2. Now try plotting 'tenyrwinpct' as the x variable, 
+                   'revenue' as the y variable (again, you have to scroll down
+                   to the bottom), and 'point' as the geom. This shows the
+                   revenue of the team (as of February 2020) versus the
+                   team's winning percentage over the past ten years. There
+                   seems to be a relatively positive correlation between the 
+                   two, and the correlation would probably be even more positive 
+                   if not for the two richest teams - the Knicks and the Lakers - 
+                   having two of the worst winning percentages over the past ten years."),
              fluidPage(
                  selectInput("x", "X variable", choices = names(nbainfo)),
                  selectInput("y", "Y variable", choices = names(nbainfo)),
-                 selectInput("label", "label", c("team", "owner")),
                  selectInput("geom", "geom", c("point", "column", "jitter")),
-                 plotOutput("plot")),
-                 
+                 plotOutput("plot"))
              )
-             
              ),
+    
+    tabPanel("Player Salary Info",
+            h2("NBA Salary Data"), 
+            DT::dataTableOutput("playercontracts")
+            ),
+    
     tabPanel("Discussion",
              titlePanel("Discussion Title"),
              p("This will be a discussion about what the model shows as well as 
              potential policy suggestions to mitigate this damage"))
 )
+
     
     # Define server logic
     server <- function(input, output, session) {
@@ -110,11 +114,13 @@ ui <- navbarPage(
                 
         }, res = 96)
         
+        output$playercontracts = DT::renderDataTable({
+            playercontracts
+        })
         
     }
     
     shinyApp(ui, server)
-    
     
     
     
